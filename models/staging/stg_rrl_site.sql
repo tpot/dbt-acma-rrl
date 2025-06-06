9dff713d61cb1a5c5e151bfe4b6ecd209c8d866d
Tim Potter <tpot@frungy.org>
2025-06-06 11:38:04 +0930

    Explicitly stage site column names

--- models/staging/stg_rrl_site.sql
+++ models/staging/stg_rrl_site.sql
@@ -1,4 +1,11 @@
 SELECT
-    * EXCLUDE (latitude, longitude),
+    site_id,
+    name,
+    state,
+    licensing_area_id,
+    postcode,
+    site_precision,
+    elevation,
+    hcis_l2,
     ST_Point(latitude, longitude) AS point_geom
 FROM {{ source('stg_rrl_site', 'stg_rrl_site') }}
